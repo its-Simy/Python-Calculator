511ee89bf12219d74f9509cef880f4aec675c36d
its-Simy <sr5428294@gmail.com>
2024-12-27 18:40:24 -0500

    more changes
    has the button layout down, now trying to fix the colors and sizing to my liking

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import PySimpleGUI as ui
 #when refering to it, you have to do ui.(whatever you wish to do) because we named it ui
 
 #window = ui.Window(title="Calculator", layout=[[]], margins=(250,150)).read()
-
+text_Color = "#000000"
+normal_Button_Color = "#FFFDD0"
+equal_Button_Color = "Dark Orange"
+outside_Buttons = {'size':(7,2), 'button_color':("black,dark grey")}
 
 layout = [
-    [ui.Text("Choose an operation")],
-    [ui.Button("+")],
-    [ui.Button("-")],
-    [ui.Button("/")],
-    [ui.Button("*")],
-    [ui.Button("Off")]
+    [ui.Text("Simple Calculator")],
+    [ui.Text("Answer Temp Placer")],
+    [ui.Button("C"),ui.Button("CE"), ui.Button("%"), ui.Button("/")],
+    [ui.Button("7"),ui.Button("8"),ui.Button("9"),ui.Button("*")],
+    [ui.Button("4"),ui.Button("5"),ui.Button("6"),ui.Button("-")],
+    [ui.Button("1"),ui.Button("2"),ui.Button("3"),ui.Button("+")],
+    [ui.Button("0", size=(7,2), button_color=("#000000,#FFFDD0")),ui.Button(".", size=(5,2),button_color=("#FFFDD0")),ui.Button("=", size=(10,2),button_color=("Dark Orange"))]
 ]
 
 #create the window
-window = ui.Window("Calculator", layout)
+window = ui.Window("Calculator", layout, margins=(100,150))
 
 #create an event loop, so if something happens, then something else has to happen
 while True:
